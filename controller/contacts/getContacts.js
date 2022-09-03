527d08876168a415cf42278a7b061454872fba04
VolodymyrPlastun <vplastun8@gmail.com>
2022-09-03 10:44:33 +0300

    fix search by favorite

--- controller/contacts/getContacts.js
+++ controller/contacts/getContacts.js
@@ -2,13 +2,12 @@ const { Contact } = require("../../models");
 
 const getContacts = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 20, favorite = false } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner: _id, favorite}, "", {
-    skip,
-    limit: Number(limit),
-    
-  }).populate("owner", "_id email subscription");
+  const contacts = await Contact.find(favorite ? { owner: _id, favorite} : { owner: _id}, "", {
+     skip,
+       limit: Number(limit),
+ }).populate("owner", "_id email subscription");
 
   res.json({
     status: "success",
